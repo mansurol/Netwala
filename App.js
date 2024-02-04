ee1575c83164fa2a8c538e820df2225e61c79848
Mansurol <mansurolislam@gmail.com>
2024-02-04 23:07:01 +0600

    final update 1.1

--- App.js
+++ App.js
@@ -1,8 +1,25 @@
+import React, { useEffect, useState } from "react";
 import { StatusBar, View } from "react-native";
 import BottomTab from "./Navigation/BottomTab";
 import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
+  const [isSplashReady, setSplashReady] = useState(false);
+
+  useEffect(() => {
+    const initializeApp = async () => {
+      await new Promise((resolve) => setTimeout(resolve, 3000));
+
+      setSplashReady(true);
+    };
+
+    initializeApp();
+  }, []);
+
+  if (!isSplashReady) {
+    return <View style={{ flex: 1, backgroundColor: "#000" }} />;
+  }
+
   return (
     <>
       <StatusBar backgroundColor="#000" />
